2635c22ffb8a8700ea16567edd0f2d89ea6e43c9
mavcr <mavcrespana@gmail.com>
2026-03-03 12:00:43 +0000

    basepath and assetprefix

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "portfolio";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
