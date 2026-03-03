c067460966b852d741d9e8d5c803417c5d5d1cbe
mavcr <mavcrespana@gmail.com>
2026-03-03 11:45:47 +0000

    fix assets

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "portfolio" // cambia si tu repo tiene otro nombre
 const nextConfig: NextConfig = {
-
-  /* config options here */
   output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
