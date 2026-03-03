00b57311e84f734e85f56a074720c352804bf7f0
mavcr <mavcrespana@gmail.com>
2026-03-03 10:47:29 +0000

    Add GitHub Actions deploy

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = "portfolio" // cambia si tu repo tiene otro nombre
 const nextConfig: NextConfig = {
+
   /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
